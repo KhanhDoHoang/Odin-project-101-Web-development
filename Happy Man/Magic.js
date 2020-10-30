2d1db3eaecf310700f937a4e15374d82bf932c9c
Hoang Do <katievincy226@gmail.com>
2020-10-30 15:59:13 -0400

    Update

--- Happy Man/Magic.js
+++ Happy Man/Magic.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Carousel set up, imported from unpkg
 // Repo: https://github.com/wethegit/wtc-controller-carousel
 const carouselEl = document.querySelector(".carousel");
@@ -90,3 +91,97 @@ const accelerateToggle = function () {
 };
 
 acceleratebtn.addEventListener("click", accelerateToggle);
+=======
+// Carousel set up, imported from unpkg
+// Repo: https://github.com/wethegit/wtc-controller-carousel
+const carouselEl = document.querySelector(".carousel");
+const myCarousel = new window.wtcControllerCarousel(carouselEl);
+const reduceMotion = window.matchMedia("(prefers-reduced-motion: reduce)")
+  .matches;
+if (reduceMotion) myCarousel.paused = true;
+
+// Fancy accelerate effect, you don't need any of this to work with the Carousel
+// Tween Repo: https://github.com/wethegit/wtc-tween
+const tween = window.wtcTween.default; // tween library
+const bars = document.querySelector(".bars");
+const acceleratebtn = document.querySelector(".accelerate-btn");
+let stepD = 0; // used to calculate frames
+let raf = null; // to save our animation
+let state = false; // to save our acceleration state
+let animating = false; /// to save the tween state
+
+// frame animation
+const step = function (now, i) {
+  // each 2 seconds call the createNewObject() function
+  if (!stepD || now - stepD >= 144) {
+    stepD = now;
+    myCarousel.offset = i * -500;
+  }
+  raf = requestAnimationFrame((d) => step(d, i + 1 > 3 ? 0 : i + 1));
+};
+
+const accelerateToggle = function () {
+  if (animating || reduceMotion) return; // don't do anything if in the middle of a tween
+  animating = true;
+  bars.classList.toggle("is-active"); // toggle bars
+
+  // accelerate! 🚀
+  if (!state) {
+    acceleratebtn.innerHTML = "Stop!";
+    tween(
+      [0, 0, 2],
+      [50, 250, 144],
+      (val) => {
+        const [leftPercent, leftOffset, speed] = val;
+
+        // this helps center the carousel
+        carouselEl.style.left = `calc(${leftPercent}% - ${leftOffset}px)`;
+        // oh yeah! ⚡️
+        myCarousel.speed = speed;
+      },
+      {
+        duration: 3000,
+        onComplete: () => {
+          // pause and reset carousel
+          myCarousel.paused = true;
+          myCarousel.offset = 0;
+          // trigger stepped animation
+          raf = requestAnimationFrame((d) => step(d, 0));
+          animating = false;
+        },
+      }
+    );
+  }
+  // decelerate 🙁
+  else {
+    acceleratebtn.innerHTML = "Accelerate";
+    // cancel our steps
+    cancelAnimationFrame(raf);
+    // reset carousel
+    myCarousel.speed = 2;
+    myCarousel.paused = false;
+
+    // nice and smooth centering
+    tween(
+      [50, 250],
+      [0, 0],
+      (val) => {
+        const [leftPercent, leftOffset] = val;
+
+        carouselEl.style.left = `calc(${leftPercent}% - ${leftOffset}px)`;
+      },
+      {
+        duration: 3000,
+        onComplete: () => {
+          animating = false;
+        },
+      }
+    );
+  }
+
+  // update state
+  state = !state;
+};
+
+acceleratebtn.addEventListener("click", accelerateToggle);
+>>>>>>> 768ede7a50cd743b4119394c60644fd61d2544db
--- iMusic/button/effect.js
+++ iMusic/button/effect.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var play = document.querySelector(".play");
 var pause = document.querySelector(".pause");
 var btn = document.querySelector(".app");
@@ -11,3 +12,18 @@ btn.addEventListener("click", () => {
     play.classList.add("active");
   }
 });
+=======
+var play = document.querySelector(".play");
+var pause = document.querySelector(".pause");
+var btn = document.querySelector(".app");
+
+btn.addEventListener("click", () => {
+  if (play.classList.contains("active")) {
+    play.classList.remove("active");
+    pause.classList.add("active");
+  } else {
+    pause.classList.remove("active");
+    play.classList.add("active");
+  }
+});
+>>>>>>> 768ede7a50cd743b4119394c60644fd61d2544db
